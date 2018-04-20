e55f43c81f02c2931842046bd6ac5aa09c5e68a1
Supriya <supriya.yerramilli@gmail.com>
2018-04-20 16:55:52 -0400

    created models

---
+++ db/migrate/20180420205237_create_assistances.rb
@@ -0,0 +1,10 @@
+class CreateAssistances < ActiveRecord::Migration[5.2]
+  def change
+    create_table :assistances do |t|
+      t.references :event, foreign_key: true
+      t.references :person, foreign_key: true
+
+      t.timestamps
+    end
+  end
+end
