b1a033f4cd6f146b4c492663a5ae934cc2ba09f8
Supriya <supriya.yerramilli@gmail.com>
2018-04-22 14:29:30 -0400

    error msg on assistance obj

--- app/controllers/api/assistances_controller.rb
+++ app/controllers/api/assistances_controller.rb
@@ -1,14 +1,14 @@
 class Api::AssistancesController < ApplicationController
 
   def index
-    # @assistances = Language.find(params[:language_id]).lessons
-    @assistances = Assistance.all
+    @assistances = Event.find(params[:event_id]).assistances
+    # @assistances = Assistance.all
     render json: @assistances
   end
 
   def show
     @assistance = Assistance.find(params[:id])
-    render json: @assistances
+    render json: @assistance
   end
 
 
