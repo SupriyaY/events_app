5c6fcb49944b6e00c3aeddbb2c77213c348f7a7a
Supriya <supriya.yerramilli@gmail.com>
2018-04-22 14:07:44 -0400

    able to see assistances object

--- app/controllers/api/assistances_controller.rb
+++ app/controllers/api/assistances_controller.rb
@@ -1,2 +1,15 @@
 class Api::AssistancesController < ApplicationController
+
+  def index
+    # @assistances = Language.find(params[:language_id]).lessons
+    @assistances = Assistance.all
+    render json: @assistances
+  end
+
+  def show
+    @assistance = Assistance.find(params[:id])
+    render json: @assistances
+  end
+
+
 end
