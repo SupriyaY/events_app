e1410181f1d59541f31d3cc93483d9b4578fd6a3
Supriya <supriya.yerramilli@gmail.com>
2018-04-21 21:13:31 -0400

    unable to view people under events

--- app/controllers/api/people_controller.rb
+++ app/controllers/api/people_controller.rb
@@ -1,14 +1,17 @@
 class Api::PeopleController < ApplicationController
-def index
+
+    def index
     @event = Event.find(params[:event_id])
 if (params[:event_id] == nil)
     @people = @event.people
     render json: @people
+end
+# @people = Person.all
+# render json: @people
 end
 
 def show
     @person = Person.find(params[:id])
     render json: @person
 end
-
 end
